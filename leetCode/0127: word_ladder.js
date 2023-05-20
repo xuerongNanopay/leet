f93f0bd1c85553c328c671d856ac2539c7919a5d
xuerongNanopay <xuerong@nanopay.net>
2023-05-20 17:35:16 -0400

    let 127

---
+++ leetCode/0127: word_ladder.js
@@ -0,0 +1,41 @@
+// 127. Word Ladder
+
+// Do not pass, because of Time Limit Exceeded
+// change to BFS, reduce repeat.
+
+var ladderLength = function(beginWord, endWord, wordList) {
+
+  let ret = Number.MAX_SAFE_INTEGER;
+
+  let doCheck = function(s1, s2) {
+    if ( s1.length !== s2.length ) return false;
+
+    let check = 0;
+    for ( let i = 0 ; i < s1.length ; i++ ) {
+      if ( s1[i] !== s2[i] ) check++;
+    }
+
+    return check === 1 ? true : false;
+  }
+
+  let process = function(cur, count) {
+    if ( cur === endWord ) {
+      ret = Math.min(ret, count+1);
+    }
+
+    let len = wordList.length;
+
+    for ( let i = 0 ; i < len ; i++ ) {
+      if ( wordList[i] !== 'xxx' && doCheck(cur, wordList[i]) ) {
+        let t = wordList[i];
+        wordList[i] = 'xxx';
+        process(t, count+1);
+        wordList[i] = t;
+      }
+    }
+  }
+
+  process(beginWord, 0);
+
+  return ret === Number.MAX_SAFE_INTEGER ? 0 : ret;
+};
