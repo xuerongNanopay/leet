dbaebff709bd37cb3e0387255d0f896973baa259
xuerongNanopay <xuerong@nanopay.net>
2023-06-05 08:57:41 -0400

    lt 33

---
+++ leetCode/0033: search_in_rotated_sorted_array.java
@@ -0,0 +1,29 @@
+// 33. Search in Rotated Sorted Array
+
+class Solution {
+  public int search(int[] nums, int target) {
+    int l = 0;
+    int r = nums.length - 1;
+
+    while ( l <= r ) {
+      int m = l + (r-l)/2;
+      if ( nums[m] == target ) return m;
+
+      if ( nums[l] <= nums[m] ) {
+        if ( nums[l] <= target && target < nums[m] ) {
+          r = m - 1;
+        } else {
+          l = m + 1;
+        }
+      } else {
+        if ( nums[r] >= target && target > nums[m] ) {
+          l = m + 1;
+        } else {
+          r = m - 1;
+        }
+      }
+    }
+
+    return -1;
+  }
+}
