b534c5ea94f00e8683d183bdcc973996d180d86a
xuerongNanopay <xuerong@nanopay.net>
2023-06-11 22:34:54 -0400

    lt 191

---
+++ leetCode/0191: number_of_1_bits.java
@@ -0,0 +1,12 @@
+// 191. Number of 1 Bits
+
+public class Solution {
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    int ret = 0;
+    for ( int i = 0 ; i < 32 ; i++ ) {
+      if ( (n >> i & 1) == 1) ret++;
+    } 
+    return ret; 
+  }
+}
