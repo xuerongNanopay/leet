685497faba0cf8fbdf1af9a3beb8a1128f2862a3
xuerongNanopay <xuerong@nanopay.net>
2023-06-11 22:51:11 -0400

    lt 136

---
+++ leetCode/0136: single_number.java
@@ -0,0 +1,11 @@
+//136. Single Number
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    int ret = 0;
+    for ( int i = 0 ; i < nums.length ; i++ ) {
+      ret ^= nums[i];
+    }
+    return ret;
+  }
+}
