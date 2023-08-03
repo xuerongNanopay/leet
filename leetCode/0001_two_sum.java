75804bd76c03e4263a051a24e10f99d16ff8078e
xuerongNanopay <xuerong@nanopay.net>
2023-08-03 19:20:17 -0400

    add name

---
+++ leetCode/0001_two_sum.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+      var cache = new HashMap<Integer, Integer>();
+
+      for ( int i = 0 ; i < nums.length ; i++ ) {
+          var cur = target - nums[i];
+          if ( cache.containsKey(cur) ) return new int[]{cache.get(cur), i};
+          cache.put(nums[i], i);
+      }
+
+      return new int[]{};
+  }
+}
